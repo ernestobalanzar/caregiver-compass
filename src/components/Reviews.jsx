00b5c74a5208ab83e2fb28c4700385b2af789466
Ernesto <ernesto.bs7@gmail.com>
2023-07-28 21:51:04 -0400

    fixing reviews
    fixing reviews title

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -269,10 +269,10 @@ export function Reviews() {
           id="reviews-title"
           className="text-3xl font-medium tracking-tight text-gray-900 sm:text-center"
         >
-          Everyone is changing their life with Pocket.
+          Everyone is changing improving their life with COMPASS.
         </h2>
         <p className="mt-2 text-lg text-gray-600 sm:text-center">
-          Thousands of people have doubled their net-worth in the last 30 days.
+          A lot of people have been using COMPASS to improve their life. Here's
         </p>
         <ReviewGrid />
       </Container>
